7453e7efe7f95111ea459c1773c96d0a883863bb
Kaustubh <162901396+deshkaustubh@users.noreply.github.com>
2025-06-23 20:09:01 +0530

    Entity created

--- app/src/main/java/com/example/busschedule/data/BusSchedule.kt
+++ app/src/main/java/com/example/busschedule/data/BusSchedule.kt
@@ -1,8 +1,12 @@
 package com.example.busschedule.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity
 data class BusSchedule(
-    val id: Int,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 100,
     val stopName: String,
     val arrivalTimeInMillis: Int
 )
